9d0d7bebb627122eb93b57a145a78e935d5b91ce
Julia Lemos Vidal <20231704@novaims.unl.pt>
2025-11-20 14:41:22 +0000

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "tictracker_v6";
-$port = 3307;
 
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
